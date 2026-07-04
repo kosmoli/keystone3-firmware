06903b6f3d5b68043e24002169d2bd124211f385
kosmoli <kosmoli@users.noreply.github.com>
2026-07-04 10:45:38 +0800

    修复 web3/gui_general_home_widgets.h include 路径残留

--- src/ui/gui_widgets/multi/gui_home_widgets.h
+++ src/ui/gui_widgets/multi/gui_home_widgets.h
@@ -5,7 +5,7 @@
 #include "lvgl.h"
 #include "gui_keyboard_hintbox.h"
 #include "gui_attention_hintbox.h"
-#include "web3/gui_general_home_widgets.h"
+#include "gui_general_home_widgets.h"
 #ifdef COMPILE_SIMULATOR
 #include "gui_pending_hintbox.h"
 #endif
